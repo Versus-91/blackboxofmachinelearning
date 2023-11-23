35401ba086a5843ebda699b5d10a3717565147d6
Ghader <gharibyan.ghader@gmail.com>
2023-11-23 23:28:07 +0100

    2nd dataset

--- test.py
+++ test.py
@@ -4,24 +4,42 @@ from sklearn.svm import SVC
 from sklearn.inspection import PartialDependenceDisplay
 import matplotlib.pyplot as plt
 
-# Load the Iris dataset
-iris = load_iris()
-X = iris.data
-y = iris.target
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
-# Fit Logistic Regression model
-logistic = LogisticRegression(max_iter=1000)
-logistic.fit(X, y)
+# Load the diabetes dataset
+diabetes = load_diabetes(as_frame=True)
+print(diabetes.head())
 
-# Fit SVM model
+X, y = diabetes.data, diabetes.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+# Normalize the features
+scaler = StandardScaler()
+X_train_normalized = scaler.fit_transform(X_train)
+X_test_normalized = scaler.transform(X_test)
+
+# Instantiate Logistic Regression model
+logreg = LogisticRegression(max_iter=2000)
+logreg.fit(X_train_normalized, y_train)
+print(X_train)
+
+# Instantiate SVM model
 svm = SVC(kernel='rbf', probability=True)
-svm.fit(X, y)
+svm.fit(X_train_normalized, y_train)
 
-# Plot partial dependence for 'petal width (cm)' feature for Logistic Regression
+# Plot partial dependence for a chosen feature index (e.g., feature 2 - body mass index) for Logistic Regression
 fig, ax = plt.subplots(figsize=(8, 6))
-PartialDependenceDisplay.from_estimator(
-    logistic, X, features = [0, 1, 2], ax=ax, target=0)
-plt.title("Partial Dependence Plot - Petal Width (Logistic Regression)")
-plt.xlabel("Petal Width (cm)")
+PartialDependenceDisplay.from_estimator(logreg, X_train_normalized,
+                                        features=[0, 1], ax=ax, target=0)
+plt.title("Partial Dependence Plot - Body Mass Index (Logistic Regression)")
+plt.xlabel("Body Mass Index")
 plt.ylabel("Partial Dependence")
 plt.show()
